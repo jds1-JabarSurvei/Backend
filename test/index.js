da3aa404527a3f255d34bf0525d0144b1e10bf91
rifaldyaristya <rifaldyaristya23@gmail.com>
2021-03-24 00:19:01 +0700

    fix TC

--- test/index.js
+++ test/index.js
@@ -10,51 +10,15 @@ test('Test correct list of forms returned', function(t){
     .expect('Content-Type', /json/)
     .expect(200)
     .end(function(err, res){
-        var expectedUsers = [
+        var expectedUsers = 
             {
                 "id": 1,
                 "title": "dummy",
                 "owner": "user"
-            },
-            {
-                "id": 2,
-                "title": "dummy2",
-                "owner": "user"
-            },
-            {
-                "id": 3,
-                "title": "dummy",
-                "owner": "user"
-            },
-            {
-                "id": 4,
-                "title": "NEW dummy",
-                "owner": "user"
-            },
-            {
-                "id": 5,
-                "title": "YUHUHUHU dummy",
-                "owner": "user"
-            },
-            {
-                "id": 6,
-                "title": "Untitled Form",
-                "owner": "user"
-            },
-            {
-                "id": 7,
-                "title": "Tes Submit API Form",
-                "owner": "user"
-            },
-            {
-                "id": 8,
-                "title": "Tes Submit API Form 2",
-                "owner": "user"
-            }
-        ];
+            };
 
         t.error(err, 'No error');
-        t.same(res.body, expectedUsers, 'List of forms as expected');
+        t.same(res.body[0], expectedUsers, 'List of forms as expected');
         t.end();
     });
 });
