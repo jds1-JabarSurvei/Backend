e4357059f307325c8670d6741683553feabd63b8
zaidannaufal <13518021@std.stei.itb.ac.id>
2021-04-26 23:16:19 +0700

    change listen frontend

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 //SETUP ALLOW CORS POLICY
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "http://polar-tundra-59366.herokuapp.com/");
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Headers",
