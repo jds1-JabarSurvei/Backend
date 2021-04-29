42845d86a101bd953530f6185548dc073f4db6bb
zaidannaufal <13518021@std.stei.itb.ac.id>
2021-04-29 14:44:23 +0700

    typo fix

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,7 +11,7 @@ const user = require('../controllers/user.controller');
 const form = require('../controllers/form.controller');
 const carousel = require('../controllers/carousel.controller');
 
-app.use(function (req, res, next) {
+router.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
