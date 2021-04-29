2936e8cf6706410076278bfcec2a7cd7898a1b11
zaidannaufal <13518021@std.stei.itb.ac.id>
2021-04-29 14:46:52 +0700

    change cors address

--- server/routes/index.js
+++ server/routes/index.js
@@ -12,7 +12,7 @@ const form = require('../controllers/form.controller');
 const carousel = require('../controllers/carousel.controller');
 
 router.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://staging-fe-jds01.herokuapp.com");
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Headers",
