4cfd5a8a6ac09a123c19f48eedd3b98ad9f7d6bc
zaidannaufal <13518021@std.stei.itb.ac.id>
2021-04-29 13:34:54 +0700

    cors

--- server/index.js
+++ server/index.js
@@ -5,26 +5,27 @@ const fileUpload = require("express-fileupload");
 const apiRouter = require("./routes");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
-// var corsOptions = {
-//   origin: 'https://polar-tundra-59366.herokuapp.com/',
-//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
-//SETUP ALLOW CORS POLICY
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "https://polar-tundra-59366.herokuapp.com/");
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
-app.use(cors({
-  origin: 'https://polar-tundra-59366.herokuapp.com',
-  credentials: false
-}))
+var corsOptions = {
+  origin: 'https://polar-tundra-59366.herokuapp.com/',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+SETUP ALLOW CORS POLICY
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://polar-tundra-59366.herokuapp.com/");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+// app.use(cors({
+//   origin: 'https://polar-tundra-59366.herokuapp.com',
+//   credentials: true
+// }))
 app.use("/", apiRouter);
 
 module.exports = app;
